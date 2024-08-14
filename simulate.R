8bf7699d37ef84359736787062fe5f0489f9a971
pixel-echo <tara.chakkithara@icloud.com>
2024-08-14 15:52:55 -0400

    simulate hail data

---
+++ simulate.R
@@ -0,0 +1,35 @@
+#### Simulate Hail Data For Canadian Locations. 
+library(tidyverse)
+set.seed(123)
+
+### Simulate the following columns:
+## Date
+## Longitude
+## Latitude
+## Size (Mean of 6cm)
+
+num_observations <- 700
+
+# Simulate Dates From June 2023 to September 2023
+start_date <- as.Date("2023-06-01")
+end_date <- as.Date("2024-09-30")
+date_sequence <- seq.Date(start_date, end_date, by = "day")
+date <- sample(date_sequence, num_observations, replace = TRUE)
+
+# Simulate Longitudes for Canada
+min_longitude <- 53
+max_longitude <- 141
+longitude <- runif(num_observations, min = min_longitude, max = max_longitude)
+
+# Simulate Latitudes for Canada
+min_latitude <- 42
+max_latitude <- 83
+latitude <- runif(num_observations, min = min_latitude, max = max_latitude)
+
+# Simulate Hail Size
+mean_value <- 7.5
+sd_value <- 6
+size <- rnorm(num_observations, mean = mean_value, sd = sd_value)
+
+hail_dataframe <- data.frame(date, longitude, latitude, size)
+write_csv(hail_dataframe, "data.csv")
